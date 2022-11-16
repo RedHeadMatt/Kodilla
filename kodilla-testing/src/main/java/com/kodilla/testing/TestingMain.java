23c7ed7e435139cf3f4a7fd39361ac744d4ea779
RedHeadMatt <41707493+RedHeadMatt@users.noreply.github.com>
2022-11-16 17:55:52 +0100

    6.2 Added com.kodilla.testing.calculator
    6.2 Added com.kodilla.testing.calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-    }
-}
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult, substractResult;
+
+        addResult = calculator.add(5, 10);
+        substractResult = calculator.substract(5, 10);
+
+        if (addResult == 15 && substractResult == -5){
+            System.out.println("calculator test OK");
+        } else {
+            System.out.println("calculator test ERROR!");
+        }
+
+
+
+    }}
