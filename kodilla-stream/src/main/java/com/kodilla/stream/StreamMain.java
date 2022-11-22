cf5b2c69c6bebccd383d6f1cdf17fe67ac63a4fd
RedHeadMatt <41707493+RedHeadMatt@users.noreply.github.com>
2022-11-22 21:03:26 +0100

    7.2 homework
    7.2 homework

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,15 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,9 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+
+    String beautify(String inputString, PoemDecorator poemDecorator){
+
+        return inputString;
+    }
+}
