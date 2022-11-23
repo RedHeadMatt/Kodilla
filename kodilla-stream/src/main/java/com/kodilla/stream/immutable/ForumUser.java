53783bae537b2e321ae56597ebe4e96738db738f
RedHeadMatt <41707493+RedHeadMatt@users.noreply.github.com>
2022-11-23 19:12:17 +0100

    7.2 homework - final added
    7.2 homework - final added

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -2,8 +2,8 @@ package com.kodilla.stream.immutable;
 
 public final class ForumUser {
 
-    private String userName;
-    private String realName;
+    private final String userName;
+    private final String realName;
 
     public ForumUser(String userName, String realName) {
         this.userName = userName;
