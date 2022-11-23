e3532bb258072b30f4c5e89bfaab729f8c9d3327
RedHeadMatt <41707493+RedHeadMatt@users.noreply.github.com>
2022-11-23 21:16:22 +0100

    7.3 homework - .filter, years -> days
    7.3 homework - .filter, years -> days

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -19,7 +19,7 @@ public class StreamMain {
 
         Map<Integer, ForumUser> listOfForumUsers = forum.getUsersList().stream()
                 .filter(forumUser -> forumUser.getMale() == 'm')
-                .filter(forumUser -> Period.between(forumUser.getDateOfBirth(), LocalDate.now()).getYears() > 20)
+                .filter(forumUser -> Period.between(forumUser.getDateOfBirth(), LocalDate.now()).getDays() > 20*365)
                 .filter(forumUser -> forumUser.getNumberOfPosts() >= 1)
                 .collect(Collectors.toMap(ForumUser::getIdentificationNumber, forumUser -> forumUser));
 
